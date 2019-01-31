bdd22fea9ed3d4fe77f98f9c0b03c9f09949ed6d
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-31 18:38:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,7 @@ def countdown (number)
   while number < 0 
   puts "#{number} SECOND(S)!"
   number -=1
-  if number == 0 
-    break 
-end
+  break if number == 0 
 end
   return "HAPPY NEW YEAR!"
 end
